6a1e997bb4b7690f26230c49ee3ab3171dd30368
水银灯、 <iqosjay@gmail.com>
2018-10-16 09:28:34 +0800

    客户端发起请求，从远端服务器获取相应信息，并返回给客户端

---
+++ plugin/wallet/proxy/.src/proxy.erl
@@ -0,0 +1,77 @@
+%%%-------------------------------------------------------------------
+%%% @author user
+%%% @copyright (C) 2018, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 15. 十月 2018 13:32
+%%%-------------------------------------------------------------------
+-module(proxy).
+-author("user").
+
+%% API
+-export([get/5,http_get/1]).
+
+
+%%从前台接受请求，与其中的值
+get([{rite,A,B},{kline,C,D,E}], _Con, Info, Headers, Body) ->
+%% 	{UID, _, _} = z_lib:get_value(Attr, ?UID, ""),
+%% 	{EID, _, _} = z_lib:get_value(Attr, ?EID, ""),
+%% 	{_AuthL, _, _} = z_lib:get_value(Attr, ?SYS_AUTH, ""),
+  %%要请求的url地址
+%  "CNY" ++ "appid=" ++ Appkey
+  io:format("!!!!!!!!!!in get ~n"),
+  io:format("!#!$!$!!!!!!!!!!!Body:~p~n",[Body]),
+  Url = z_lib:get_value(Body, "url", []),
+  io:format("!#!$!$!!!!!!!!!!!Url:~p~n",[Url]),
+  %%获取当前类型
+  Type = z_lib:get_value(Body, "type", []),
+  io:format("!#!$!$!!!!!!!!!!!Type:~p~n",[Type]),
+  if
+    %%取汇率
+     Type == "rite" ->
+       %%整形转字符串
+    %    Appkey = integer_to_list(A),
+     %   Sign   = integer_to_list(B),
+       io:format("!#!$!$!!!!!!!!!!!A:~p~n",[A]),
+       io:format("!#!$!$!!!!!!!!!!!B:~p~n",[B]),
+     Urlnow=Url++"&appkey="++A++"&sign="++B,
+    io:format("!!!!!!!Urlnow:~p~n",[Urlnow]);
+    %% 取K线图
+    Type == "kline" ->
+      %Period = integer_to_list(C),
+     % Size = integer_to_list(D),
+      %AccessKeyId = integer_to_list(E),
+      Urlnow=Url++"&period="++C++"&size="++D++"&AccessKeyId="++E,
+      io:format("!!!!!!!Urlnow:~p~n",[Urlnow])
+  end,
+  %%调用自身方法，从服务器远端获取当前数据
+  {RC, Data} = http_get(Urlnow),
+  io:format("!!!!!!!Data:~p~n",[Data]),
+  NewHeaders=zm_http:set_header(zm_http:resp_headers(Info, Headers, ".txt", none), "Access-Control-Allow-Origin", "*"),
+  %%代理向客户端发送请求，返回当前结果,读取到的结果放回到Content中
+  {ok, [{code, RC} | Info], NewHeaders, Data}.
+
+
+http_get(Url) ->
+  % 启动网络环境
+  Result = inets:start(),
+  if
+    (Result =:= ok) orelse (Result =:= {error, {already_started, inets}}) ->
+      try httpc:request(get, {Url, [{"connection", "keep-alive"}]}, [{timeout, 5000}], [{body_format, binary}]) of
+        {ok, {{_, RC, _}, _, Body}} when RC >= 200, RC < 400 ->
+          {RC, Body};
+        E ->
+          erlang:throw({500, [{errcode, 500},{errmsg, "send result error"}, {result, lists:flatten(io_lib:write(E))}, {url, Url}]})
+      catch
+        _: Reason ->
+          erlang:throw({500, [{errcode, 500},{errmsg, "send error"}, {result, lists:flatten(io_lib:write(Reason))}, {url, Url}]})
+      end;
+    true ->
+      erlang:throw({500, [{errcode, 500},{errmsg, "inets start error"}, {result, lists:flatten(io_lib:write(Result))}, {url, Url}]})
+  end.
+
+
+
+
+
